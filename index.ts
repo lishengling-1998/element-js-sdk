11a141526fb674f64bcbd0fcc89cc4b7cddc8d0e
lsl <>
2024-03-25 15:14:22 +0800

    no message

--- index.ts
+++ index.ts
@@ -2,41 +2,33 @@ export { ElementSDK } from './src/index'
 export { ethers, providers, Signer, BigNumber } from 'ethers'
 export type { TransactionResponse, TransactionReceipt } from '@ethersproject/abstract-provider'
 
-export type {
-    OrderQuery
-} from './src/api/openApiTypes'
+export type { OrderQuery } from './src/api/openApiTypes'
 
-export {
-    NULL_ADDRESS,
-    ETH_TOKEN_ADDRESS,
-    Network,
-    OrderSide,
-    SaleKind,
-    Standard,
-    Market
-} from './src/types/types'
+export { NULL_ADDRESS, ETH_TOKEN_ADDRESS, Network, OrderSide, SaleKind, Standard, Market } from './src/types/types'
 
 export type {
-    SignerOrProvider,
-    Asset,
-    ElementAPIConfig,
-    OrderInformation,
-    Order,
-    OrderDetail,
-    GasParams,
-    ERC721SellOrderItem,
-    MakeERC721SellOrdersParams,
-    FailedERC721Item,
-    MakeERC721SellOrdersResponse,
-    MakeOrderParams,
-    FillOrderParams,
-    BatchBuyWithETHParams,
-    EncodeTradeDataParams,
-    CancelOrderParams,
-    CancelOrdersParams,
-    CancelOrdersResponse,
-    CancelAllOrdersByMakerParams
+  SignerOrProvider,
+  Asset,
+  ElementAPIConfig,
+  OrderInformation,
+  Order,
+  OrderDetail,
+  GasParams,
+  ERC721SellOrderItem,
+  MakeERC721SellOrdersParams,
+  FailedERC721Item,
+  MakeERC721SellOrdersResponse,
+  MakeOrderParams,
+  FillOrderParams,
+  BatchBuyWithETHParams,
+  EncodeTradeDataParams,
+  CancelOrderParams,
+  CancelOrdersParams,
+  CancelOrdersResponse,
+  CancelAllOrdersByMakerParams
 } from './src/types/types'
 
 export { RPC_URLS } from './src/contracts/config'
 export { getChain, getChainId } from './src/util/chainUtil'
+export { encodeBatchSignedOrders } from './src/swap/encodeBatchSignedOrders'
+export { Swap } from './src/swap/swap'
